6cab9974f8a311cff9abcc009c26a080b746b387
cabello <milan.cabello@gmail.com>
2023-12-04 12:56:20 +0100

    ajout de js et id dans index

---
+++ assets/js/addcart.js
@@ -0,0 +1,9 @@
+let panier = [];
+
+document.getElementById('BVoiture1').addEventListener('click',function() {
+
+    let nomproduit = document.getElementById('Voiture1').value;
+    let PrixProduit = ducoment.getElementById('PrixVoiture1').value;
+
+})
+
