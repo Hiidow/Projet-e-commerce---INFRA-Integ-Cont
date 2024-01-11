307cd6a3b9907e04fb02e1f00fb37a8f29b71117
Maxence <maxence.poizat@etu.uca.fr>
2024-01-11 10:10:49 +0100

    Début panier

--- assets/js/addcart.js
+++ assets/js/addcart.js
@@ -4,6 +4,18 @@ document.getElementById('BVoiture1').addEventListener('click',function() {
 
     let nomproduit = document.getElementById('Voiture1').value;
     let PrixProduit = ducoment.getElementById('PrixVoiture1').value;
+    let quantite = document.getElementById('QuantiteVoiture1').value;
 
+    let produit = {
+        nom: nomproduit,
+        Prix: PrixProduit,
+        quantite: quantite
+    }
+
+    panier.push(produit);
+    console.log(panier);
+
+    let produitJSON = JSON.stringify(panier);
+
+    localStorage.setItem('panier', produitJSON);
 })
-
